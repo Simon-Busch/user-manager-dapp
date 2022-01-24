a216df60e40c72b6b1f6db0b3c47c51cd9a6b25a
simonbusch <Simon.busch@live.fr>
2022-01-24 07:46:53 +0100

    [UPD] improve user creation testing

--- test/user-manager-tests.js
+++ test/user-manager-tests.js
@@ -57,7 +57,7 @@ describe("UserManager contract test -------------", function () {
   });
 
   describe("User creation", function () {
-    it("Should be able to create a user", async function () {
+    it("Should have a user with all properties", async function () {
       await userManagerContract.createUser(
         "bob",
         "bob",
@@ -65,13 +65,34 @@ describe("UserManager contract test -------------", function () {
         "bob@bob.bob",
         22,
         "QmdocV9tSr7qvRe3qmC3J7AwWw4D5pj8gnMPnWrneZjJfD",
-        "https://github.com/Simon-Busch",
+        "https://github.com/",
+        "worker"
+      );
+      const user = await userManagerContract.getUser(1) 
+      const userList = await userManagerContract.getAllUsers();
+      expect(user.id).to.equal(1);
+      expect(user.name).to.equal("bob");
+      expect(user.lastName).to.equal("bob");
+      expect(user.telephoneNumber).to.equal(5454545454);
+      expect(user.personalLink).to.equal("https://github.com/");
+      expect(user.age).to.equal(22);
+      expect(user.ipfsHash).to.equal("QmdocV9tSr7qvRe3qmC3J7AwWw4D5pj8gnMPnWrneZjJfD");
+      expect(user.tags[0]).to.equal("worker");
+      expect(userList.length).to.equal(2);
+    });
+    it("User list should have a length of 2 after user creation", async function () {
+      await userManagerContract.createUser(
+        "bob",
+        "bob",
+        5454545454,
+        "bob@bob.bob",
+        22,
+        "QmdocV9tSr7qvRe3qmC3J7AwWw4D5pj8gnMPnWrneZjJfD",
+        "https://github.com/",
         "worker"
       );
       const userList = await userManagerContract.getAllUsers();
       expect(userList.length).to.equal(2);
     });
-  })
-
-
+  });
 });
