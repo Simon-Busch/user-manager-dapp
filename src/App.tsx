8cd439db9ecead4c918d85337bb30ed2e915c5df
Simon Busch <simonbusch@macbook-pro-de-simon.home>
2022-01-24 11:59:08 +0100

    [UPD] dependencies, clean up basic css in Index and App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="user-manager__main-container">
+      <h1 className="main-title">User Manager</h1>
+      
+    </div>
+  );
+}
+
+export default App;
