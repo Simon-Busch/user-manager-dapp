3c9b63169bb5b0c2e5a0cc844a000934b274f127
simonbusch <Simon.busch@live.fr>
2022-01-24 06:07:37 +0100

    [UPD] changer contract Name, clean tests, update hardhatconfig

---
+++ scripts/run.js
@@ -0,0 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const UserManager = await hre.ethers.getContractFactory("UserManager");
+  const userManagerContract = await UserManager.deploy();
+
+  await userManagerContract.deployed();
+  console.log("userManager deployed to:", userManagerContract.address);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
