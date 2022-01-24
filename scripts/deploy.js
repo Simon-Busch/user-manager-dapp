ad943825529d58d43511b0fd6c3e9abc296b3de1
simonbusch <Simon.busch@live.fr>
2022-01-24 07:54:20 +0100

    [ADD] deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const UserManager = await hre.ethers.getContractFactory("UserManager");
+  const userManagerContract = await UserManager.deploy();
+  await userManagerContract.deployed();
+  console.log("userManager deployed to:", userManagerContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
