81fe4b05638f426d9e3ff56d9ca057d7b6ee4431
simonbusch <Simon.busch@live.fr>
2022-01-24 13:34:33 +0100

    [ADD] user model and use accordingly, create dummy data to test functionnality, [UPD] userList and user component

---
+++ src/model/User.model.ts
@@ -0,0 +1,11 @@
+export interface UserModel {
+  id: number;
+  name: string;
+  lastName: string;
+  telephoneNumber: number;
+  email: string;
+  age: number;
+  ipfsHash: string;
+  personalLink: string;
+  tags: string[];
+}
