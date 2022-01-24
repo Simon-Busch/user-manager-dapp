42f5d5887cdad2b4d21c11cdb43548c116922769
simonbusch <Simon.busch@live.fr>
2022-01-24 06:23:28 +0100

    [ADD] basic structure of a user, [ADD] all empty functions for the needed actions with users

--- contracts/UserManager.sol
+++ contracts/UserManager.sol
@@ -4,5 +4,44 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract UserManager {
+    struct User {
+        uint id;
+        string name;
+        string lastName;
+        uint telephoneNumber;
+        string email;
+        uint age;
+        string ipfsHash;
+        string[] tags;
+    }
 
+
+    User public user;
+    User[] public users;
+
+    uint public userId;
+
+    constructor() {
+        userId = 0;
+    }
+
+    function getAllUsers() external view returns (User[] memory) {
+
+    }
+
+    function getUser(uint _id) public view returns(User memory) {
+
+    }
+
+    function createUser(string memory _name, string memory _lastName, uint _telephoneNumber, string memory  _email, uint  _age, string memory _ipfsHash, string memory _tag) public {
+
+    }
+
+    function updateUser(uint id, string memory _name, string memory _lastName, uint _telephoneNumber, string memory  _email, uint  _age, string memory _ipfsHash, string[] memory _tag) public {
+
+    }
+
+    function deleteUser(uint _id) public {
+
+    }
 }
