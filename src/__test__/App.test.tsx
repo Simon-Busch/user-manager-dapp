0264f862d7dca0f0b70f1efcf4386e05e719caed
Simon Busch <simonbusch@macbook-pro-de-simon.home>
2022-01-25 16:49:38 +0100

    delete app test

--- src/__test__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders title User Manager', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/User Manager/i);
-  expect(titleElement).toBeInTheDocument();
-});
